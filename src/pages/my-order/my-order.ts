97071c2e5d39a51b06361494c5498f82905bb3d9
Michal Rozumny <rozumny@hotmail.com>
2017-12-31 18:15:34 +0100

    fix my orders

--- src/pages/my-order/my-order.ts
+++ src/pages/my-order/my-order.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { OrderService } from "../../services/order-service";
 import { SigninService } from "../../services/signin-service";
 import { FileService } from "../../services/file-service";
+import { ItemService } from "../../services/item-service";
 import { ModalService } from "../../services/modal-service";
 import { Utils } from "../../services/utils-service";
 
@@ -17,11 +18,23 @@ export class MyOrderPage {
     public nav: NavController,
     public modalService: ModalService,
     public orderService: OrderService,
+    private itemService: ItemService,
     public signinService: SigninService,
     public fileService: FileService
   ) {
     this.modalService.showWait(orderService.getAll().then(data => {
-      this.orders = data.filter(x => x.userId === this.signinService.user._id);
+      // this.orders = data.filter(x => x.userId === this.signinService.user._id);
+      let promises = [];
+      promises = data.map(order => {
+        order.items.forEach(item => {
+          return this.itemService.getItem(item.item.key).then(y => {
+            item.item.filenames = y.filenames;
+          });
+        });
+      })
+      return Promise.all(promises).then(() => {
+        this.orders = data;
+      });
     }));
   }
 
